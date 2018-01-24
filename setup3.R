f567202cfd9e2c12c4edbd09f5139f69ef810fdf
DragonflyStats <kobriendublin@gmail.com>
2018-01-24 17:06:32 +0000

    Update setup3.R

--- setup3.R
+++ setup3.R
@@ -1,7 +1,9 @@
 install.packages("readr")
 install.packages("modelr")
 install.packages("randomForest")
+install.packages("ggplot2")
 
 library(readr)
 library(modelr)
 library(randomForest)
+library(ggplot2)
